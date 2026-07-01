098dcb33bf3ed5f100f2f3ebe7975227c66ee9fc
⚓️ <atefalsaadouny@alMacBook-Pro-alkhas-bAtef.local>
2026-07-01 19:41:07 +0300

    Increase register throttle from 5 to 20 per minute

--- routes/auth.php
+++ routes/auth.php
@@ -15,7 +15,7 @@ Route::middleware(['guest', 'throttle:60,1'])->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
         ->name('register');
 
-    Route::post('register', [RegisteredUserController::class, 'store'])->middleware('throttle:5,1');
+    Route::post('register', [RegisteredUserController::class, 'store'])->middleware('throttle:20,1');
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
